d96f179318b15f3878acc05a8535f637e5e41626
DorferThomas <1810653831@stud.fh-kufstein.ac.at>
2018-11-26 14:18:17 +0100

    Aufgabe2SchritteZählerHinzugef.

--- src/Aufgabe2_PKZ.java
+++ src/Aufgabe2_PKZ.java
@@ -5,6 +5,7 @@ public class Aufgabe2_PKZ {
     {
         int zahl1 = Integer.parseInt(JOptionPane.showInputDialog("Geben Sie eine Zahl ein: "));
         int zahl2 = Integer.parseInt(JOptionPane.showInputDialog("Geben Sie eine Zahl ein: "));
+        int schritte = 0;
 
         //Falls zwei indentische Zahlen eingegeben werden, wird Zahl 1 direkt in die Konsole ausgegeben.
         if (zahl1==zahl2)
@@ -19,6 +20,10 @@ public class Aufgabe2_PKZ {
             {
                 //Zur simulierung könnte man die Zahl nach jeden Durchgang ausgeben lassen.     System.out.println(zahl2);
                 zahl2++;
+
+                //Schritte zählen
+                schritte++;
+
             }
             //Falls die Zahl 2 größer ist, wird die Zahl 1 immer um eins erhöht.
 
@@ -26,10 +31,13 @@ public class Aufgabe2_PKZ {
             {
                 //Zur simulierung könnte man die Zahl nach jeden Durchgang ausgeben lassen.     System.out.println(zahl1);
                 zahl1++;
+
+                //Schritte zählen
+                schritte++;
             }
         }
 
-        System.out.println( "Die Zahl ist " + zahl1);
+        System.out.println( "Die höhere Zahl war: " + zahl1 + ". Es benötigte " + schritte + " Schritte bis die Zahl ermittelt werden konnte.");
 
     }
 }
