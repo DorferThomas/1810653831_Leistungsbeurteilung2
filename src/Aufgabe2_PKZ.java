3750931786300b11d21c805fc3dcc3d7ea1b63e5
DorferThomas <1810653831@stud.fh-kufstein.ac.at>
2018-11-26 14:13:17 +0100

    Aufgabe2Zahl1UndZahl2VergleichenUndAusgeben

---
+++ src/Aufgabe2_PKZ.java
@@ -0,0 +1,35 @@
+import javax.swing.*;
+
+public class Aufgabe2_PKZ {
+    public static void main(String[] args)
+    {
+        int zahl1 = Integer.parseInt(JOptionPane.showInputDialog("Geben Sie eine Zahl ein: "));
+        int zahl2 = Integer.parseInt(JOptionPane.showInputDialog("Geben Sie eine Zahl ein: "));
+
+        //Falls zwei indentische Zahlen eingegeben werden, wird Zahl 1 direkt in die Konsole ausgegeben.
+        if (zahl1==zahl2)
+        {
+            System.out.println("Die Zahl war: " + zahl1);
+
+        }else
+        {
+            //Falls die Zahl 1 größer ist, wird die Zahl 2 immer um eins erhöht.
+            //Die while Schleife läuft so lange bis die sie nicht mehr war ist.
+            while (zahl1>zahl2)
+            {
+                //Zur simulierung könnte man die Zahl nach jeden Durchgang ausgeben lassen.     System.out.println(zahl2);
+                zahl2++;
+            }
+            //Falls die Zahl 2 größer ist, wird die Zahl 1 immer um eins erhöht.
+
+            while (zahl1<zahl2)
+            {
+                //Zur simulierung könnte man die Zahl nach jeden Durchgang ausgeben lassen.     System.out.println(zahl1);
+                zahl1++;
+            }
+        }
+
+        System.out.println( "Die Zahl ist " + zahl1);
+
+    }
+}
---
+++ src/Main.java
@@ -0,0 +1,14 @@
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+        int a = 10;
+        int b = 1;
+
+        while (a>b){
+            System.out.println(a);
+            a--;
+        }
+    }
+
+}
